53ee976005dbde300e1fd90c61dfb7f79c7181b2
sh4nnongoh <sh4nnongoh@gmail.com>
2018-12-27 16:22:41 +0800

    testing circuitbreaker

--- examples/githubstats/main.go
+++ examples/githubstats/main.go
@@ -4,12 +4,19 @@ import (
 	//"log"
 	"net/http"
 	"os"
+	"fmt"
 
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/circuitbreaker"
+	"github.com/sony/gobreaker"
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/sh4nnongoh/goGithubStats/src/githubstats"
 )
+
+func testStateChange(name string, from, to gobreaker.State){
+	fmt.Println(name + " changed ")
+}
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
@@ -18,6 +25,16 @@ func main() {
 
 	var generateReport endpoint.Endpoint
 	generateReport = githubstats.MakeGenerateReportEndpoint(svc)
+	settings := gobreaker.Settings{
+		Name: "test",
+		MaxRequests: 1,
+		Interval: 1000000,
+		Timeout: 3000000,
+		ReadyToTrip: nil,
+		OnStateChange: testStateChange,
+	}
+	fmt.Println(settings)
+	generateReport = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{}))(generateReport)
 	generateReport = githubstats.LoggingMiddleware(log.With(logger, "method", "generateReport"))(generateReport)
 
 	generateReportHandler := httptransport.NewServer(
