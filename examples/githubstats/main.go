aa7b8a5a96c2245d7f682d3ea541b8e5d846a31a
sh4nnongoh <sh4nnongoh@gmail.com>
2019-01-02 15:07:58 +0800

    added prometheus instumentation

--- examples/githubstats/main.go
+++ examples/githubstats/main.go
@@ -2,48 +2,73 @@ package main
 
 import (
 	//"log"
+	"fmt"
 	"net/http"
 	"os"
-	"fmt"
 
-	"github.com/go-kit/kit/endpoint"
-	"github.com/go-kit/kit/circuitbreaker"
-	"github.com/sony/gobreaker"
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sh4nnongoh/goGithubStats/src/githubstats"
+	"github.com/sony/gobreaker"
 )
 
-func testStateChange(name string, from, to gobreaker.State){
+func testStateChange(name string, from, to gobreaker.State) {
 	fmt.Println(name + " changed ")
 }
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
 
-	svc := githubstats.NewGithubReportService()
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "github_report_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "github_report_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "github_report_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{}) // no fields here
 
-	var generateReport endpoint.Endpoint
-	generateReport = githubstats.MakeGenerateReportEndpoint(svc)
-	settings := gobreaker.Settings{
-		Name: "test",
-		MaxRequests: 1,
-		Interval: 1000000,
-		Timeout: 3000000,
-		ReadyToTrip: nil,
-		OnStateChange: testStateChange,
-	}
-	fmt.Println(settings)
-	generateReport = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{}))(generateReport)
-	generateReport = githubstats.LoggingMiddleware(log.With(logger, "method", "generateReport"))(generateReport)
+	var svc githubstats.GithubReportService
+	svc = githubstats.NewGithubReportService()
+	svc = githubstats.NewLoggingMiddleware(logger, svc)
+	svc = githubstats.NewInstrumentingMiddleware(requestCount, requestLatency, countResult, svc)
+
+	// var generateReport endpoint.Endpoint
+	// generateReport = githubstats.MakeGenerateReportEndpoint(svc)
+	// settings := gobreaker.Settings{
+	// 	Name:          "test",
+	// 	MaxRequests:   1,
+	// 	Interval:      1000000,
+	// 	Timeout:       3000000,
+	// 	ReadyToTrip:   nil,
+	// 	OnStateChange: testStateChange,
+	// }
+	// fmt.Println(settings)
+	//generateReport = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{}))(generateReport)
+	//generateReport = githubstats.LoggingMiddleware(log.With(logger, "method", "generateReport"))(generateReport)
 
 	generateReportHandler := httptransport.NewServer(
-		generateReport,
+		githubstats.MakeGenerateReportEndpoint(svc),
 		githubstats.DecodeGenerateReportRequest,
 		githubstats.EncodeResponse,
 	)
 
 	http.Handle("/generateReport", generateReportHandler)
+	http.Handle("/metrics", promhttp.Handler())
 	logger.Log("msg", "HTTP", "addr", ":8080")
 	logger.Log("err", http.ListenAndServe(":8080", nil))
 }
--- src/githubstats/middleware.go
+++ src/githubstats/middleware.go
@@ -1,18 +1,48 @@
 package githubstats
 
 import (
-	"context"
+	"time"
 
-	"github.com/go-kit/kit/endpoint"
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/metrics"
 )
 
-func LoggingMiddleware(logger log.Logger) endpoint.Middleware {
-	return func(next endpoint.Endpoint) endpoint.Endpoint {
-		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			logger.Log("msg", "calling endpoint")
-			defer logger.Log("msg", "called endpoint")
-			return next(ctx, request)
-		}
-	}
+// func LoggingMiddleware(logger log.Logger) endpoint.Middleware {
+// 	return func(next endpoint.Endpoint) endpoint.Endpoint {
+// 		return func(ctx context.Context, request interface{}) (interface{}, error) {
+// 			logger.Log("msg", "calling endpoint")
+// 			defer logger.Log("msg", "called endpoint")
+// 			return next(ctx, request)
+// 		}
+// 	}
+// }
+
+func NewInstrumentingMiddleware(
+	requestCount metrics.Counter,
+	requestLatency metrics.Histogram,
+	countResult metrics.Histogram,
+	next GithubReportService) InstrumentingMiddleware {
+
+	return instrumentingMiddleware{requestCount, requestLatency, countResult, next}
+}
+
+type InstrumentingMiddleware interface {
+	GenerateReport(username, token string, repository []string) (output []repository)
+}
+
+type instrumentingMiddleware struct {
+	requestCount   metrics.Counter
+	requestLatency metrics.Histogram
+	countResult    metrics.Histogram
+	next           GithubReportService
+}
+
+func (mw instrumentingMiddleware) GenerateReport(username, token string, repository []string) (output []repository) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "generateReport", "error", "nil"}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	output = mw.next.GenerateReport(username, token, repository)
+	return
 }
